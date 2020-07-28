4a2e4ba18f8e799af627f8eaee45853adeaca1de
Henrique Derosa <henriquederosa@Henriques-MacBook-Pro.local>
2020-07-27 21:52:36 -0300

    feat: Setup firebase/auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, StatusBar, SafeAreaView, Text} from 'react-native';
-import SignIn from './pages/SignIn';
 import {NavigationContainer} from '@react-navigation/native';
+import auth from '@react-native-firebase/auth';
+
+import SignIn from './pages/SignIn';
 import AuthRoutes from './routes';
 import colors from '../colors';
 
 const App: React.FC = () => {
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  // Handle user state changes
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  if (initializing) return <SafeAreaView style={{flex: 1}}><Text>Loading</Text></SafeAreaView>;
+
   return (
     <NavigationContainer>
       <StatusBar barStyle="light-content" backgroundColor={colors.background} />
